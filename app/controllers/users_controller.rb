01dedd437fa8a7f3cdb0d91622621db29252fe78
dmitrydk <slipin2020@mail.ru>
2020-11-25 00:18:14 +0300

    Add view partially

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = "Привет!"
+    @user = User.new(
+    name: "Dima",
+    username: "dack",
+    avatar_url: "https://goodprogrammer.ru/system/avatars/000/016/134/503d2ca27264c2567d5cde553587451d98539fc1_x300.jpg?1602616080"
+    )
   end
 end
