cb957b4b732d760309c10b36cb70750f4a2245e6
dmitrydk <slipin2020@mail.ru>
2020-11-26 20:57:50 +0300

    JS does not operate

--- app/javascript/packs/click.js
+++ app/javascript/packs/click.js
@@ -1,6 +1,11 @@
-$(function(){
-    $('#ask-button').click(function(){
-        $('#ask-form').slideToggle(300);
-        return false;
-    });
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const askButton = document.getElementById('ask-button')
+    const askForm = document.getElementById('ask-form')
+    askButton.addEventListener('click', (event) => {
+        event.preventDefault()
+        setTimeout(() => {
+            let displayStyle = askForm.style.display === 'block' ? 'none' : 'block'
+            askForm.style.display = displayStyle
+        }, 300)
+    })
+})
