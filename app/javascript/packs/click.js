04e033ea5bba4b1482434c73c67d20d5601f7096
dmentry <slipin2020@mail.ru>
2021-01-27 23:45:15 +0300

    Cosmetic editings 2

--- app/javascript/packs/click.js
+++ app/javascript/packs/click.js
@@ -14,15 +14,3 @@ document.addEventListener('turbolinks:load', () => {
         })
     }
 })
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const askButton = document.getElementById('ask-button')
-//     const askForm = document.getElementById('ask-form')
-//     askButton.addEventListener('click', (event) => {
-//         event.preventDefault()
-//         setTimeout(() => {
-//             let displayStyle = askForm.style.display === 'block' ? 'none' : 'block'
-//             askForm.style.display = displayStyle
-//         }, 300);
-//     })
-// })
