11276e61164ab424467b81857665a0af9e2068f9
dmentry <slipin2020@mail.ru>
2021-02-26 00:03:47 +0300

    Add screenshot and readme

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,6 +1,6 @@
 class HashtagsController < ApplicationController
   def show
-    # локальная переменная
+    # локальная переменная, т.к. не нужна во вьюхе
     hashtag = Hashtag.with_questions.find_by!(text: params[:text])
     @questions = hashtag.questions
   end
